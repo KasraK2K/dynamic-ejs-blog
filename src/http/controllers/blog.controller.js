fdd5f6ded935a992678f2c7faa823e83503f982b
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-03 12:20:25 +0330

    send data as object in controller

--- src/http/controllers/blog.controller.js
+++ src/http/controllers/blog.controller.js
@@ -3,7 +3,7 @@ const {blogPostDataService} = require("../services/blog.service")
 
 const blogPostController = async (req, res) => {
   const data = await blogPostDataService()
-  return res.render('app', data)
+  return res.render('app', { data })
 }
 
 module.exports = {
