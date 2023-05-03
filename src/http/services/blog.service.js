f63cb10887a16de8c37deb2474431de7e33f8924
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-03 12:20:02 +0330

    change path and add dynamic_id for each element

--- src/http/services/blog.service.js
+++ src/http/services/blog.service.js
@@ -8,17 +8,18 @@ const blogPostDataService = async () => {
   for (const element of data.elements) {
     const basePath = path.resolve(
       process.cwd(),
-      `statics/vendors/${element.component}/${element.component}`
+      `statics/vendors/components/${element.component}/${element.component}`
     )
-    const baseAddress = `${global.serverAddress}/vendors/${element.component}/${element.component}`
+    const baseAddress = `${global.serverAddress}/vendors/components/${element.component}/${element.component}`
 
     const stylePath = `${baseAddress}.css`
     const scriptPath = `${baseAddress}.js`
 
     if (fs.existsSync(`${basePath}.css`)) element.style = stylePath
     if (fs.existsSync(`${basePath}.js`)) element.script = scriptPath
-  }
 
+    element.dynamic_id = (+new Date() + Math.floor(Math.random() * (999 - 100) + 100)).toString(16)
+  }
   return data
 }
 
