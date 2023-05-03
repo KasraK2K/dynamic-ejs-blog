1ef9197ae1b8b22c42dfcf190afb636b52aac8b9
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-03 08:30:15 +0330

    add nodemon

---
+++ nodemon.js
@@ -0,0 +1,11 @@
+const nodemon = require("nodemon");
+
+nodemon({ script: "src/server.js" })
+  .on("start", console.clear)
+  .on("restart", console.clear)
+  .on("crash", () => console.error("\nApplication has crashed!\n"))
+  .on("quit", () => {
+    console.log("\nApp has quit\n");
+    process.kill(process.pid, "SIGKILL");
+    process.exit();
+  });
