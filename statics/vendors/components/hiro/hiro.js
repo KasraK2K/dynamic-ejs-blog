5563c55a84347d9ff335de4120a0bf12bc42b571
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-04 09:31:15 +0330

    change hiro script

--- statics/vendors/components/hiro/hiro.js
+++ statics/vendors/components/hiro/hiro.js
@@ -1,11 +1,14 @@
 // @ts-nocheck
 /* NOTE that all css and js are global so be careful on choosing name for functions and variables */
 
-const HIRO__SERVER_DATA_SENT = $('#behavior_script').data('object')
-const state = {}
-
+/* -------------------------------------------------------------------------- */
+/*                                Hiro Behavior                               */
+/* -------------------------------------------------------------------------- */
 $(document).ready(() => {
-  for (const element of HIRO__SERVER_DATA_SENT.elements) {
+  const SERVER_DATA_SENT = $('#behavior_script').data('object')
+  const state = {}
+
+  for (const element of SERVER_DATA_SENT.elements) {
     if (element.component === 'hiro') {
       state[element.dynamic_id] = +$(`#${element.dynamic_id} .hiro_btn`).text()
       $(`#${element.dynamic_id} .hiro_btn`).on('click', () => {
