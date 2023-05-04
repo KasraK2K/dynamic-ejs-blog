fb60f3a345b3ff8e08799c4df480ac846cff8aa8
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-04 11:49:05 +0330

    add webpack build config file

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// @ts-nocheck
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+const WebpackShellPluginNext = require('webpack-shell-plugin-next')
+
+module.exports = {
+  entry: {
+    server: './src/server.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  target: 'node',
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false, // and __filename return blank or /
+  },
+  externals: [nodeExternals()], // Need this to avoid error when working with Express
+  module: {
+    rules: [
+      {
+        // Transpiles ES6-8 into ES5
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'web' }]],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+            cacheDirectory: true,
+          },
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackShellPluginNext({
+      onAfterDone: {
+        scripts: ["echo 'Build complete!'"],
+      },
+    }),
+  ],
+}
