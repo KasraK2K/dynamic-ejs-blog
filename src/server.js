1b2b23f7fb0a00708fd8ad3df5ba381a7cf1d5ff
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-04 11:49:44 +0330

    use lodash and connect everything

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const compression = require('compression')
+const _ = require('lodash')
 const app = express()
-const v1 = require("./http/routes/v1")
-require("./configuration")
-
+const v1 = require('./http/routes/v1')
+require('./configuration')
 
 const port = process.env.PORT || 8000
 const serverAddress =
   process.env.NODE_ENV === 'production'
     ? 'firstdash.com'
     : process.env.NODE_ENV === 'development'
-      ? 'dev.firstdash.com'
-      : `http://localhost:${port}`
-Object.assign(global, { port, serverAddress })
+    ? 'dev.firstdash.com'
+    : `http://localhost:${port}`
+_.assign(global, { port, serverAddress })
 
 app.set('view engine', 'ejs')
 app.set('views', './src/views/pages')
@@ -22,45 +22,5 @@ app.use(express.urlencoded({ extended: true }))
 app.use(compression())
 app.use(express.static('statics'))
 app.use('/v1', v1)
-
-
-// app.get('/:company/:id', (req, res) => {
-//   const data = {
-//     title: 'App Page',
-//     app: {
-//       title: 'Application Page',
-//     },
-//     elements: [
-//       {
-//         component: 'hiro',
-//         configuration: {
-//           title: 'This is a Hiro!',
-//         },
-//       },
-//       {
-//         component: 'slider',
-//         configuration: {
-//           images: ['image_one', 'image_two', 'image_three'],
-//         },
-//       },
-//     ],
-//   }
-
-//   for (const element of data.elements) {
-//     const basePath = path.resolve(
-//       process.cwd(),
-//       `statics/vendors/${element.component}/${element.component}`
-//     )
-//     const baseAddress = `${serverAddress}/vendors/${element.component}/${element.component}`
-
-//     const stylePath = `${baseAddress}.css`
-//     const scriptPath = `${baseAddress}.js`
-
-//     if (fs.existsSync(`${basePath}.css`)) element.style = stylePath
-//     if (fs.existsSync(`${basePath}.js`)) element.script = scriptPath
-//   }
-
-//   return res.render('app', data)
-// })
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
