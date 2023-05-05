be5d2415bb0161a1355490527d3621f2d62ce7a7
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-05 10:46:35 +0330

    add server address to environments

--- src/configuration/requarments.js
+++ src/configuration/requarments.js
@@ -1,6 +1,6 @@
 const assert = require('assert').strict
 
-const REQUIRED_ENVIREMENTS = ['NODE_ENV', 'MONGO_URI']
+const REQUIRED_ENVIREMENTS = ['NODE_ENV', 'MONGO_URI', 'PORT', 'SERVER_ADDRESS']
 
 for (const key of REQUIRED_ENVIREMENTS)
   assert.ok(process.env[key], `The ${key} environment variable is required`)
