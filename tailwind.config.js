ac3ca8727d574b99f09402c311211fa13370c136
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-06 20:21:51 +0330

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ejs,css}', './statics/**/*.{ejs,css}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
