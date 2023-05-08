02de685e690967eee15e6d6e9b512b2b24df7226
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-08 08:48:56 +0330

    emit values by click on btn

---
+++ statics/vendors/components/grid/grid.js
@@ -0,0 +1,12 @@
+// $(document).ready(() => {
+//   for (const element of SERVER_DATA_SENT.elements) {
+//     $(`section#${element.dynamic_id} .lifestyle_btn`).on('click', () => {
+//       $emit('change-element', element.dynamic_id, 1, 2, ['a', 'b', 'c'])
+//     })
+//   }
+// })
+
+function gridEmitter(dynamic_id) {
+  const element = _.find(SERVER_DATA_SENT.elements, { dynamic_id })
+  $emit('change-element', dynamic_id, element)
+}
