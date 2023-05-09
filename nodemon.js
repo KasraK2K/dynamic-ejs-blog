7c7bb17cd10a253ce9e3e2502ef0e15080bd97c1
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-09 15:35:55 +0330

    modify nodemon

--- nodemon.js
+++ nodemon.js
@@ -1,11 +1,11 @@
-const nodemon = require("nodemon");
+const nodemon = require('nodemon')
 
-nodemon({ script: "src/server.js" })
-  .on("start", console.clear)
-  .on("restart", console.clear)
-  .on("crash", () => console.error("\nApplication has crashed!\n"))
-  .on("quit", () => {
-    console.log("\nApp has quit\n");
-    process.kill(process.pid, "SIGKILL");
-    process.exit();
-  });
+nodemon({ script: 'src/server.js' })
+  .on('start', console.clear)
+  .on('restart', console.clear)
+  .on('crash', () => console.error('\nApplication has crashed!\n'))
+  .on('quit', () => {
+    console.log('\nApp has quit\n')
+    process.kill(process.pid, 'SIGKILL')
+    process.exit()
+  })
