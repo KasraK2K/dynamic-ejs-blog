bfcb2fd8b6c76705f8da3fa4c19b6c181f16308b
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-09 15:38:36 +0330

    edit pages title and more complete dialogs

--- statics/vendors/components/lifestyle/lifestyle.js
+++ statics/vendors/components/lifestyle/lifestyle.js
@@ -10,3 +10,13 @@ function lifeStyleEmitter(dynamic_id) {
   const element = _.find(SERVER_DATA_SENT.elements, { dynamic_id })
   $emit('change-element', dynamic_id, element)
 }
+
+function closeModifyDialog(dynamic_id) {
+  $(`#dialog_${dynamic_id}`).dialog('close')
+}
+
+function publishModifyDialog(dynamic_id) {
+  const title = $(`#title_${dynamic_id}`).val()
+
+  closeModifyDialog(dynamic_id)
+}
