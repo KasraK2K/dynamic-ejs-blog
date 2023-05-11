b623eca62a3fc0bbf542cc29002b62ab43aab244
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-11 14:11:25 +0330

    add functions for edit external links

--- statics/vendors/components/lifestyle/lifestyle.js
+++ statics/vendors/components/lifestyle/lifestyle.js
@@ -16,7 +16,27 @@ function closeModifyDialog(dynamic_id) {
 }
 
 function publishModifyDialog(dynamic_id) {
-  const title = $(`#title_${dynamic_id}`).val()
+  const title = $(`#title_${dynamic_id}`)
+
+  console.log(title.val())
 
   closeModifyDialog(dynamic_id)
+}
+
+function changeExternalLink(event, dynamic_id, index) {
+  // change state on change (this function)
+  // replace SERVER_DATA_SENT to state on cancel (cancel btn function)
+  // change SERVER_DATA_SENT by using state and save on publish (publish btn  function)
+
+  const element = _.find(SERVER_DATA_SENT.elements, { dynamic_id })
+  const type = event.target.dataset.type
+  if (type === 'label') {
+    element.configuration.external_links[index].label = event.target.value
+    $emit('change-element', dynamic_id, element)
+  } else if (type === 'link') {
+    element.configuration.external_links[index].link = event.target.value
+    $emit('change-element', dynamic_id, element)
+  } else {
+    // show error toast
+  }
 }
