908ade4ccec1889c2569c8ab525ba2828f63d9b9
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-12 16:18:08 +0330

    add default dialog

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const withMT = require('@material-tailwind/html/utils/withMT')
 const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
-module.exports = withMT({
+module.exports = {
   content: ['./src/**/*.{ejs,js,sass,scss,css}'],
   theme: {
     extend: {
@@ -12,4 +12,4 @@ module.exports = withMT({
     },
   },
   plugins: [plugin(({ addBase, addComponents, addUtilities, theme, e, config }) => {})],
-})
+}
