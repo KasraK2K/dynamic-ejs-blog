52d26df2368247d1f62d2bb21783f96d7eda6ce9
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-12 16:21:59 +0330

    make life style editable

--- statics/vendors/components/lifestyle/lifestyle.js
+++ statics/vendors/components/lifestyle/lifestyle.js
@@ -6,7 +6,8 @@
 //   }
 // })
 
-function lifeStyleEmitter(dynamic_id) {
+function lifeStyleButton(dynamic_id) {
+  if (SERVER_DATA_SENT.editable) return
   const element = _.find(SERVER_DATA_SENT.elements, { dynamic_id })
   $emit('change-element', dynamic_id, element)
 }
