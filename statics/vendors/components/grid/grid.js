499e3b0f59ca035c6deb58e5da3c751e4d1e4d62
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-12 16:20:15 +0330

    change grid function

--- statics/vendors/components/grid/grid.js
+++ statics/vendors/components/grid/grid.js
@@ -7,6 +7,7 @@
 // })
 
 function gridEmitter(dynamic_id) {
+  if (SERVER_DATA_SENT.editable) return
   const element = _.find(SERVER_DATA_SENT.elements, { dynamic_id })
   $emit('change-element', dynamic_id, element)
 }
