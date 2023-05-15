a68069258cbf9009f6d3cf167d0ba6733ad8683a
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-15 20:57:51 +0330

    fix tailwind config bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,16 @@ const withMT = require('@material-tailwind/html/utils/withMT')
 const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
-module.exports = withMT({
-  content: ['./src/**/*.{ejs,js,sass,scss,css}'],
+module.exports = {
+  content: ['./src/**/*.{ejs,js,sass,scss,css}', './statics/**/*.{ejs,js,sass,scss,css}'],
   theme: {
     extend: {
       screens: {},
-      colors: {},
+      colors: {
+        'deep-pb1': '#0F1637',
+      },
       fontFamily: {},
     },
   },
   plugins: [plugin(({ addBase, addComponents, addUtilities, theme, e, config }) => {})],
-})
+}
