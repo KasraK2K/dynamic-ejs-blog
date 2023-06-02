1e2f5cf063b44f352a78ca593a78a49a1ce1abe5
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-02 14:43:42 +0330

    add sentry

--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ const cors = require('cors')
 const _ = require('lodash')
 const responseTime = require('response-time')
 const { restResponseTimeHistogram } = require('./common/prometheus/metrics')
-const registerPrometheus = require('./common/prometheus/register')
-const startMetricsServer = require('./common/prometheus/register')
+const startMetricsServer = require('./common/prometheus')
+const Sentry = require('@sentry/node')
 require('./configuration')
 
 _.assign(global, {})
@@ -22,6 +22,31 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(compression())
 app.use(cors())
+
+Sentry.init({
+  enabled: true,
+  environment: process.env.NODE_ENV,
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    // Automatically instrument Node.js libraries and frameworks
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
+// RequestHandler creates a separate execution context, so that all
+// transactions/spans/breadcrumbs are isolated across requests
+app.use(Sentry.Handlers.requestHandler())
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler())
+
 app.use(
   responseTime((req, res, time) => {
     if (req?.route?.path) {
@@ -42,6 +67,16 @@ app.use(express.static('statics'))
 app.use('/components', express.static('src/views/components'))
 app.use(express.static('uploads'))
 app.use('/v1', v1)
+
+// Sentry
+app.use(Sentry.Handlers.errorHandler())
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500
+  res.end(res.sentry + '\n')
+})
 
 // Prometheus Sub App
 startMetricsServer(process.env.METRIC_PORT)
